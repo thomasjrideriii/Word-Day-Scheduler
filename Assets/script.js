2489d7cbac021c1ec4251bad1bd74a78dedeab42
Thomas Rider <thomasjrideriii@gmail.com>
2020-05-09 16:51:44 -0400

    Added day functionality and buttons saving to local storage.

---
+++ Assets/script.js
@@ -0,0 +1,29 @@
+$("#currentDay").text(moment().format("LLLL"))
+
+function init(){
+    for (var i = 15; i < 18; i++) {
+        $("#txt"+i).val(localStorage.getItem(i))
+    }
+}
+
+for (var i = 9; i < 18; i++) {
+    if (i < moment().hour()) {
+        $("#txt"+i).addClass("past")
+    } else if (i === moment().hour()) {
+        $("#txt"+i).addClass("present")
+    } else {
+        $("#txt"+i).addClass("future")
+    }
+}
+
+$("button").click(function(event){
+    event.preventDefault()
+    var hourNum = this.id
+    var slot = this.id
+    localStorage.setItem(slot, $("#txt"+hourNum).val())
+    console.log(this.id)
+    console.log($("#txt"+hourNum).val())
+    console.log(localStorage.getItem(slot))
+})
+
+init()
